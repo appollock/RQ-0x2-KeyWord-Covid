e55eee02c40af6c3c2104b7c2b0b29a7deb65b80
LazzR <41189421+appollock@users.noreply.github.com>
2021-03-12 14:47:37 +0100

    Implement Person model class

--- GenericCovidObserver/Model/Person.cs
+++ GenericCovidObserver/Model/Person.cs
@@ -2,6 +2,27 @@
 {
     public class Person
     {
+        private readonly int age;
+        private bool isHealthy;
 
+        public int Age
+        {
+            get { return age; }
+        }
+        public bool IsHealthy
+        {
+            get { return isHealthy; }
+            set { isHealthy = value; }
+        }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+
+        public Person(int age, bool isHealthy, string name, string surname)
+        {
+            this.age = age;
+            IsHealthy = isHealthy;
+            Name = name;
+            Surname = surname;
+        }
     }
 }
