11b7a149e011f28f39a8ab8b9099af9c8d9ec217
LazzR <41189421+appollock@users.noreply.github.com>
2021-03-12 15:49:30 +0100

    Implement PersonController and IObserver

--- GenericCovidObserver/Observer/PersonController.cs
+++ GenericCovidObserver/Observer/PersonController.cs
@@ -1,7 +1,30 @@
-﻿namespace GenericCovidObserver.Observer
-{
-    public class PersonController
-    {
+﻿using GenericCovidObserver.Model;
+using System;
 
+namespace GenericCovidObserver.Observer
+{
+    public class PersonController : IObserver<CovidCell>
+    {
+        private Person person;
+
+        public PersonController(Person person)
+        {
+            this.person = person;
+        }
+
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(CovidCell covidCell)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
